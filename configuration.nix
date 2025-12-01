e754a578f0b757a921e73208d30dfb549f6fc541
Ryan Harris <ShiftyMcCool@users.noreply.github.com>
2025-11-30 21:06:22 -0500

    Set up NixOS configuration with SSH and Git
    Configure system packages, SSH, and Git settings.

---
+++ configuration.nix
@@ -0,0 +1,52 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQDWAHZ07y/ome3ioXmlQpWP+7NsY9+UKG8/AT0pCgB macbook"
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Ryan Harris";
+    userEmail = "ryan@it-clowd.top";
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "prohibit-password";
+    };
+    hostKeys = [
+      {
+        path = "/etc/ssh/forgejo";
+        type = "ed25519";
+      }
+    ];
+  };
+
+  programs.ssh = {
+    extraConfig = "
+      Host forgejo
+        HostName forgejo.it-clowd.top
+        Port 2223
+        User git
+        IdentityFile /etc/ssh/forgejo
+        IdentitiesOnly yes
+    ";
+  };
+
+  system.stateVersion = "25.05";
+}
